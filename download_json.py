74a4557aa460914ffb97d20feace2df82e65f320
njanetos <nejanetos@gmail.com>
2015-11-18 19:22:25 -0500

    Fixed directory issue.

--- download_json.py
+++ download_json.py
@@ -2,12 +2,12 @@ import os
 import json
 from pprint import pprint
 
-root = os.path.join('data', 'source_urls')
+root = os.path.join('data')
 
 for f in os.listdir(root):
 
     # Get the index file
-    indices = [ g for g in os.listdir(os.path.join('data', 'source_urls', f)) if os.path.isfile(os.path.join('data', 'source_urls', f, g))]
+    indices = [ g for g in os.listdir(os.path.join('data', f)) if os.path.isfile(os.path.join('data', f, g))]
 
     if len(indices) != 1:
         print "Failed to find index file at " + f
@@ -19,7 +19,7 @@ for f in os.listdir(root):
         print "Failed to recognize index file at " + f
         continue
 
-    urls = open(os.path.join('data', 'source_urls', f, indices)).read()
+    urls = open(os.path.join('data', f, indices)).read()
 
     try:
         urls_json = json.loads(urls)
@@ -27,21 +27,23 @@ for f in os.listdir(root):
         print "Failed to parse json index at " + f
         continue
 
-    if not os.path.exists(os.path.join('data', 'source_urls', f, 'formulary_urls')):
-        os.makedirs(os.path.join('data', 'source_urls', f, 'formulary_urls'))
-    if not os.path.exists(os.path.join('data', 'source_urls', f, 'plan_urls')):
-        os.makedirs(os.path.join('data', 'source_urls', f, 'plan_urls'))
-    if not os.path.exists(os.path.join('data', 'source_urls', f, 'provider_urls')):
-        os.makedirs(os.path.join('data', 'source_urls', f, 'provider_urls'))
+    print "Downloading JSON data for " + f
+
+    if not os.path.exists(os.path.join('data', f, 'formulary_urls')):
+        os.makedirs(os.path.join('data', f, 'formulary_urls'))
+    if not os.path.exists(os.path.join('data', f, 'plan_urls')):
+        os.makedirs(os.path.join('data', f, 'plan_urls'))
+    if not os.path.exists(os.path.join('data', f, 'provider_urls')):
+        os.makedirs(os.path.join('data', f, 'provider_urls'))
 
     for httpurl in urls_json['formulary_urls']:
         os.system('wget ' + httpurl)
-        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', 'source_urls', f, 'formulary_urls') + '/')
+        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', f, 'formulary_urls') + '/')
 
     for httpurl in urls_json['plan_urls']:
         os.system('wget ' + httpurl)
-        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', 'source_urls', f, 'plan_urls') + '/')
+        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', f, 'plan_urls') + '/')
 
     for httpurl in urls_json['provider_urls']:
         os.system('wget ' + httpurl)
-        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', 'source_urls', f, 'provider_urls') + '/')
+        os.system('mv ' + httpurl.split('/')[-1] + ' ' + os.path.join('data', f, 'provider_urls') + '/')
